5dfb2ba3020c04d115c0a9c9e4f5f0cf178b332b
CharaTime-sys <775196511@qq.com>
2023-07-08 09:12:51 +0800

    第一次

---
+++ Assets/Scripts/ButtonExtension.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public enum Button_Press_Type
+{
+    Short,
+    Long,
+}
+
+public class ButtonExtension : MonoBehaviour
+{
+    public float pressmaxTime = 1;
+
+    public bool isDown = false;
+    public bool isExit = false;
+    private float downTime = 0f;
+
+    private Button_Press_Type type;
+
+    void Update()
+    {
+        //按下之后开始计时
+        if (isDown)
+        {
+            downTime += Time.deltaTime;
+        }
+        if (downTime >= pressmaxTime)
+        {
+            type = Button_Press_Type.Long;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        //设置按下状态
+        isDown = true;
+        //设置点击状态
+        type = Button_Press_Type.Short;
+    }
+
+    private void OnMouseUp()
+    {
+        //设置抬起状态
+        isDown = false;
+        Press_Finished();
+        downTime = 0;
+    }
+
+    private void OnMouseExit()
+    {
+        //如果不是在按下的时候鼠标移出去的话就不进行判断
+        if (!isDown)
+        {
+            return;
+        }
+        Press_Finished();
+        //设置变量
+        isDown = false;
+        isExit = true;
+    }
+    /// <summary>
+    /// 点击完成后
+    /// </summary>
+    private void Press_Finished()
+    {
+        if (isExit)
+        {
+            isExit = false;
+            return;
+        }
+        if (type == Button_Press_Type.Long)
+        {
+            Debug.Log(type + downTime.ToString());
+        }
+        else
+        {
+            Debug.Log(type + downTime.ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/Global/SceneController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
